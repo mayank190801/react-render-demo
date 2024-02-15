64b8edc8c793be26a88ecfa924a98f2a37d8d315
Mayank Singh <mayank190801@gmail.com>
2024-02-16 00:04:34 +0530

    start with basics again

---
+++ test.js
@@ -0,0 +1,16 @@
+
+console.log("hello world")
+//this works pretty well 
+
+for(let i = 0; i < 4; i++){
+    console.log(i + " hey man");
+}
+
+
+console.log(String(213412).length)
+
+console.log(("213") + 123);
+
+//create one really cool and simple habit tracker for sure - (do it brother)
+//simple habit tracker - which people enjoy using and what not!!! - simple and beautiful is important - 
+
